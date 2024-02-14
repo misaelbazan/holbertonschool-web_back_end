6828a1f38d3b5deef16e705a591085bdfe45650b
misaelbazan <misaelbfranco@gmail.com>
2024-02-13 21:59:00 -0500

    6th task file

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains one function"""
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """This function takes a list of ints and floats and
+    returns their sum as float
+    Args:
+        mxd_lst: List[Union[int, float]] - should be a
+        list that only contains both int, float
+    Returns:
+        float(sum(mxd_lst)) - sum of elements in mxd_lst as float
+    """
+    return float(sum(mxd_lst))
