079d4d1666b10de28371ec61ade4f86ba7ecc14e
misaelbazan <misaelbfranco@gmail.com>
2024-02-13 19:57:01 -0500

    1-concat.py file

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This module contains one function"""
+
+def concat(str1: str, str2: str) -> str:
+    """This function returns a concatenated string
+    Args:
+        - str1: str
+        - str2: str
+    Return:
+        str(str1+str2)
+    """
+    return str(str1+str2)
