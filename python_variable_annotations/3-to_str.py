d49f58205f226c6f347db5fa254822265fd202d5
misaelbazan <misaelbfranco@gmail.com>
2024-02-13 21:18:54 -0500

    3-to_Str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""this module contains one function"""
+
+
+def to_str(n: float) -> str:
+    """This function takes a float and returns its
+    string representation
+    Args:
+        n: float
+    Return:
+        str(n)
+    """
+    return str(n)
