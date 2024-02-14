bc78c7e5a888e9e7d14ce0097165768c11c7ebb9
misaelbazan <misaelbfranco@gmail.com>
2024-02-13 21:40:06 -0500

    5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This functions contains one function"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """This function takes a list of floats and returns
+    their sum as float
+    Args:
+        input_list: List[float]
+    Return:
+        float(sum(input_list))
+    """ 
+    return float(sum(input_list))
