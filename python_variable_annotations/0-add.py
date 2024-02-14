d595ad583974ba42fa74386126335647c1ed377f
misaelbazan <misaelbfranco@gmail.com>
2024-02-13 19:22:39 -0500

    0-add.py file

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This module contains one function"""
+
+
+def add(a: float, b: float) -> float:
+  """This function takes two arguments and return the
+  adition of both
+  Args:
+    - a: float
+    - b: float
+  Return:
+    float(a + b)
+  """
+  return float(a + b)
