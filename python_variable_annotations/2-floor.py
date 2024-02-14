7b5a3933c508082c8dee083204458169d26156f5
misaelbazan <misaelbfranco@gmail.com>
2024-02-13 20:59:40 -0500

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This module contains one function"""
+
+
+def floor(n: float) -> int:
+  """This function takes a float and returns it floor
+  Args:
+    n: float
+  Return:
+    int(n // 1)
+  """
+  return int(n // 1)
