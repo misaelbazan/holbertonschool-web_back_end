0c46e2046b9b7d6fa41fe2260ccae3e1f513126c
misaelbazan <misaelbfranco@gmail.com>
2024-02-22 20:50:02 -0500

    1st task file

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,6 +2,7 @@
 """This module contains an async coroutine"""
 
 from typing import List
+import asyncio
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
@@ -16,7 +17,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         the delays should be in ascending order without using
         sort() because of concurrency
     """
-    tasks = [asyncio.create_task(wait_random(max_delay))
-        for _ in range(n)]
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     completed_ts = await asyncio.gather(*tasks)
     return sorted(completed_ts)
