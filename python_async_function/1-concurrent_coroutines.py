955ca040f94c7c8e3fa9c6ba905af08ef929c34f
misaelbazan <misaelbfranco@gmail.com>
2024-02-25 21:36:52 -0500

    1st task file

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,23 +2,22 @@
 """This module contains an async coroutine"""
 
 from typing import List
-
-wait_random = __import__('0-basic_async_syntax').wait_random
+import asyncio
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Async coroutine that takes in 2 int args, and will
+    spawn wait_random x n times with the specified "max_delay"
+    Args:
+        n: int - number of times wait_random will spawn
+        max_delay: int = the maximum delay in seconds
+    Return:
+        The list of all delays (float values). The list of
+        the delays should be in ascending order without using
+        sort() because of concurrency
     """
-    Wait for a list of random delays.
-
-    Arguments:
-    - n: number of random delays to wait
-    - max_delay: the maximum value that a delay can have
-
-    Returns:
-    - List of delay times, in ascending order
-    """
-    delay_list = []
-    for _ in range(n):
-        delay = await wait_random(max_delay)
-        delay_list.append(delay)
-    return sorted(delay_list)
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    completed_ts = await asyncio.gather(*tasks)
+    return sorted(completed_ts)
+    
