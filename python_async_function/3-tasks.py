7e8ca0b7a2582aa064adfd33560a328eae2c3272
misaelbazan <misaelbfranco@gmail.com>
2024-02-25 21:29:36 -0500

    3rd task file

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains a function
+"""
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+import asyncio
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """This function creates and returns and asyncronous task
+    Args:
+        max_delay: int - max delay to be passed to the wait_random function
+    Return:
+        task - the task create when wait_random() was called with create_class()
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
