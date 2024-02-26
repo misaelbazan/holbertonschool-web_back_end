0db8c4cfef1ddeb1eec8bcc8c82d8823650b8ebe
misaelbazan <misaelbfranco@gmail.com>
2024-02-25 21:38:48 -0500

    2nd task file

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,9 +2,9 @@
 """This module contains a function
 """
 
-wait_n = __import__("1-concurrent_coroutines").wait_n
 import time
 import asyncio
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
