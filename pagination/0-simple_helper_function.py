3f66924eeddc11caa43c6504b681bc4b5e9700a8
misaelbazan <misaelbfranco@gmail.com>
2024-03-05 20:29:22 -0500

    0 task file

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -14,6 +14,6 @@ def index_range(page: int, page_size: int) -> Tuple:
     Returns:
         A tuple containing the start index and the end index
     """
-    start_index = (page - 1) * page_size 
+    start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
