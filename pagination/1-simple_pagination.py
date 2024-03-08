e6c242d23c0c39fc321f1c416a80c64e832468f9
misaelbazan <misaelbfranco@gmail.com>
2024-03-07 16:54:49 -0800

    1st task files

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -4,8 +4,21 @@
 
 import csv
 import math
-from typing import List
-index_range = __import__("0-simple_helper_function").index_range
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """This function takes two arguments
+    Args:
+        page: int - the page number to retrieve
+        page_size: int - limit, specifies the number of items
+            that should be returned in a single page
+    Returns:
+        A tuple containing the start index and the end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
 
 
 class Server:
