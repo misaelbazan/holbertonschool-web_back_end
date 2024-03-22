6a55d8e52e71cbfbdcec6e9ebdeee052d6e5fd8f
misaelbazan <misaelbfranco@gmail.com>
2024-03-21 17:25:49 -0700

    1st task file

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,13 +1,13 @@
 import ClassRooms from "./0-classroom.js";
 
 function initializeRooms() {
-  const class_room1 = new ClassRooms(19);
-  const class_room2 = new ClassRooms(20);
-  const class_room3 = new ClassRooms(34);
+  const classRoom1 = new ClassRooms(19);
+  const classRoom2 = new ClassRooms(20);
+  const classRoom3 = new ClassRooms(34);
 
-  const array_class = [class_room1, class_room2, class_room3];
+  const arrayClass = [classRoom1, classRoom2, classRoom3];
 
-  return array_class;
+  return arrayClass;
 }
 
 export default initializeRooms;
