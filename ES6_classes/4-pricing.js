2bbc1023b563ed59e2b24160ec8c2084cbc02e7e
misaelbazan <misaelbfranco@gmail.com>
2024-03-24 19:57:16 -0700

    4th task file

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+// Import the class <Currency> from 3-currency.js
+// Implement a class named <Pricing>
+
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // Getter and Setter for _currency
+  get currency() {
+    return this.currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+}
