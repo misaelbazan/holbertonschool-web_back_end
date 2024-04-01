414ff16dd4e91cff2bab3c836a843b284c24f068
misaelbazan <misaelbfranco@gmail.com>
2024-03-31 19:48:22 -0700

    10th task file

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let index = 0;
+  const newArray = [];
   for (const value of array) {
-    array[index] = appendString + value;
-    index++;
-  };
-  return array;
+    newArray.push(appendString + value);
+  }
+  return newArray;
 }
