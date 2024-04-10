4ed28cdbc30c38faabd51fedca52adf2b6bad0d7
misaelbazan <misaelbfranco@gmail.com>
2024-04-10 02:35:24 +0000

    0 task file

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+// Return a Promise
+
+export default function getResponseFromAPI() {
+	const myPromise = new Promise((resolve, reject) => {
+		resolve('Success');
+	});
+	return myPromise;
+}
---
+++ ES6_promise/README.md
@@ -0,0 +1,2 @@
+README.md
+0-promise.js
