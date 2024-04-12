8433235bb926b6eb8feef74b08d8334e1e0ed60d
misaelbazan <misaelbfranco@gmail.com>
2024-04-12 01:33:06 +0000

    lint observances corrected

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 // Return a Promise
 export default function getResponseFromAPI() {
-	const myPromise = new Promise((resolve, reject) => {
-		resolve('Success');
-	});
-	return myPromise;
+  const myPromise = new Promise((resolve) => {
+    resolve('Success');
+  });
+  return myPromise;
 }
--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -2,7 +2,7 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
-      resolve({ status: 200, body: 'Success'});
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
