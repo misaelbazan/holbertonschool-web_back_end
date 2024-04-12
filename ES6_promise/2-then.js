67da50ae6ada9e8c1cb85263267098e31d153818
misaelbazan <misaelbfranco@gmail.com>
2024-04-12 02:05:30 +0000

    2nd task files

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      const response = { status: 200, body: 'success' };
+      return response;
+    })
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
