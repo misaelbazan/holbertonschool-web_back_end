711e073206e70266806109492b08c87318c1ca88
misaelbazan <misaelbfranco@gmail.com>
2024-04-13 00:49:38 +0000

    6th task files

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(fileName);
+  const promises = [promise1, promise2];
+
+  Promise.allSettled(promises)
+    .then((results) => {
+      results.forEach((result) => console.log(result));
+    });
+}
