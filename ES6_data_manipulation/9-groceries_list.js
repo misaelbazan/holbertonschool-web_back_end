0f33706a5b6251a29c12dbad2ffd372f1e122f96
misaelbazan <misaelbfranco@gmail.com>
2024-04-22 03:20:40 +0000

    9th task file

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+// Create a function named <<groceriesList>> that returns a map
+export default function groceriesList() {
+  const groceriesMap = new Map();
+  groceriesMap.set('Apples', 10);
+  groceriesMap.set('Tomatoes', 10);
+  groceriesMap.set('Pasta', 1);
+  groceriesMap.set('Rice', 1);
+  groceriesMap.set('Banana', 5);
+
+  return groceriesMap;
+}
