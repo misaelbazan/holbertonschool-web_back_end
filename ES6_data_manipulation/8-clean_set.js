ec4f3ef8791a6f5f8b2a7b4eb1e7929abfa12d91
misaelbazan <misaelbfranco@gmail.com>
2024-04-22 03:01:11 +0000

    8th task files

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+// Create a function named <<cleanSet>> that returns a string of all the set values
+//    that start with a specific string
+// Accepts two arguments
+// When a value starts with <<startString>> you only append the rest of the string
+export default function cleanSet(setToCheck, startString) {
+  if (!startString) return '';
+  const setToClean = setToCheck;
+  const listResult = [];
+  for (const elem of setToClean) {
+    if (elem.startsWith(startString)) {
+      listResult.push(elem.substr(startString.length));
+    }
+  }
+  return listResult.join('-');
+}
